bd58cc7c64b7bf13a21049a1163110149ae911c8
John Haste <joaovictorspteixeira@gmail.com>
2022-05-21 02:44:11 -0300

    UI Basic Interaction Added

---
+++ Assets/InputSystemTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputSystemTest : MonoBehaviour
+{
+    void Attack(){
+        print("Attack");
+    }
+}
